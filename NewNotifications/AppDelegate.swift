d6a10134029fdf020350b2ae89250c7965a6b465
p4vlos <nicolaou_paul@hotmail.com>
2016-09-15 13:16:13 +0300

    added attachment to notification

--- NewNotifications/AppDelegate.swift
+++ NewNotifications/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //to show the notifications while app is running
+        UNUserNotificationCenter.current().delegate = self
+        
         return true
     }
 
@@ -44,3 +49,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+
+//to show the notifications while app is running
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler(.alert)
+    }
+}
--- NewNotifications/ViewController.swift
+++ NewNotifications/ViewController.swift
@@ -7,19 +7,70 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 1. REQUESST PERMISSION
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound], completionHandler: { (granted, error) in
+         
+            if granted {
+                print("Notification access granted")
+            } else {
+                print(error?.localizedDescription)
+            }
+        })
+        
+    }
+    
+    @IBAction func notifyButtonTapped(sender: UIButton) {
+        
+        scheduleNotification(inSeconds: 5) { (success) in
+            
+            if success {
+                print("Successfully scheduled notification")
+            } else {
+                print("Error scheduling notification")
+            }
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func scheduleNotification(inSeconds: TimeInterval, completion: @escaping ( _ Success: Bool) -> ()) {
+        
+        //Add an attachment
+        let myImage = "IMG"
+        guard let imageUrl = Bundle.main.url(forResource: myImage, withExtension: "jpeg") else {
+            completion(false)
+            return
+        }
+        
+        var attachment: UNNotificationAttachment
+        
+        attachment = try! UNNotificationAttachment(identifier: "myNotification", url: imageUrl, options: .none)
+        
+        let notif = UNMutableNotificationContent()
+        
+        notif.title = "New Notification"
+        notif.subtitle = "Those are great!"
+        notif.body = "The new notification option in iOS 10 are what I've always dreamed of"
+        
+        notif.attachments = [attachment]
+        
+        let notifTrigger = UNTimeIntervalNotificationTrigger(timeInterval: inSeconds, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "myNotification", content: notif, trigger: notifTrigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+            if error != nil {
+                print(error)
+                completion(false)
+            } else {
+                
+                completion(true)
+            }
+        })
     }
-
-
 }
-
